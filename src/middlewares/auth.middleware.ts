b8fb25e24b1bd0e2dda447d1225a667af2c5d4de
KiptooMannu <Kkgg7241@gmail.com>
2024-06-12 16:42:35 +0300

    users auth updates done succesfully

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,31 +1,30 @@
-
 import "dotenv/config";
 import { verify } from "hono/jwt";
 import { Context, Next } from "hono";
-import { ne } from "drizzle-orm";
+
 
 interface HonoRequest<T, U> {
     user?: T;
     // Add other properties if needed
 }
 
-// AUTHRNICATION MIDDLEWARE
+// AUTHENTICATION MIDDLEWARE
 export const verifyToken = async (token: string, secret: string) => {
     try {
         const decoded = await verify(token as string, secret);
         return decoded;
-
     } catch (error: any) {
         return null;
     }
 }
 
-// AUTHENTICATION MIDDLEWARE
+// AUTHORIZATION MIDDLEWARE
 export const authMiddleware = async (c: Context & { req: HonoRequest<any, unknown> }, next: Next, requiredRole: string) => {
     const token = c.req.header("Authorization");
 
     if (!token) return c.json({ error: "Token is required" }, 401);
-    const decoded = await verifyToken(token as string, process.env.JWT_SECRET as string);
+
+    const decoded = await verifyToken(token.replace('Bearer ', ''), process.env.JWT_SECRET as string);
 
     if (!decoded) return c.json({ error: "Invalid token" }, 401);
 
@@ -41,7 +40,6 @@ export const authMiddleware = async (c: Context & { req: HonoRequest<any, unknow
 
     return c.json({ error: "Unauthorized" }, 401);
 }
-
 
 export const adminRoleAuth = async (c: Context, next: Next) => await authMiddleware(c, next, "admin");
 export const userRoleAuth = async (c: Context, next: Next) => await authMiddleware(c, next, "user");
--- src/users/user.router.ts
+++ src/users/user.router.ts
@@ -1,32 +1,4 @@
 
-// import { Hono } from 'hono';
-// import { getUsersController, getUserByIdController, createUserController, updateUserController, deleteUserController } from './user.controller';
-// import { zValidator } from '@hono/zod-validator';
-// // Import the userSchema from the validators module
-// import { userSchema } from '../validator';
-// import { adminRoleAuth, userRoleAuth, bothRoleAuth } from './../middlewares/auth.middleware';
-
-// export const userRouter = new Hono();
-
-// // get all users
-// userRouter
-//     .get("users", userRoleAuth, getUsersController)
-//     .post("users", zValidator('json', userSchema, (result, c) => {
-//         if (!result.success) {
-//             return c.json(result.error, 400);
-//         }
-//     }), createUserController)
-
-// // get user by id
-// userRouter
-//     .get("users/:id", bothRoleAuth, getUserByIdController)
-//     .put("users/:id", zValidator('json', userSchema, (result, c) => {
-//         if (!result.success) {
-//             return c.json(result.error, 400);
-//         }
-//     }), updateUserController)
-//     .delete("users/:id", deleteUserController);
-
 import { Hono } from 'hono';
 import { getUsersController, getUserByIdController, createUserController, updateUserController, deleteUserController } from './user.controller';
 import { zValidator } from '@hono/zod-validator';
