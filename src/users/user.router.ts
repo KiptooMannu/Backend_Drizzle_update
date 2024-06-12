7d6ecab108ed721bbb959b05658295b475ea8340
KiptooMannu <Kkgg7241@gmail.com>
2024-06-12 15:25:09 +0300

    changes

--- src/users/user.router.ts
+++ src/users/user.router.ts
@@ -1,28 +1,58 @@
 
+// import { Hono } from 'hono';
+// import { getUsersController, getUserByIdController, createUserController, updateUserController, deleteUserController } from './user.controller';
+// import { zValidator } from '@hono/zod-validator';
+// // Import the userSchema from the validators module
+// import { userSchema } from '../validator';
+// import { adminRoleAuth, userRoleAuth, bothRoleAuth } from './../middlewares/auth.middleware';
+
+// export const userRouter = new Hono();
+
+// // get all users
+// userRouter
+//     .get("users", userRoleAuth, getUsersController)
+//     .post("users", zValidator('json', userSchema, (result, c) => {
+//         if (!result.success) {
+//             return c.json(result.error, 400);
+//         }
+//     }), createUserController)
+
+// // get user by id
+// userRouter
+//     .get("users/:id", bothRoleAuth, getUserByIdController)
+//     .put("users/:id", zValidator('json', userSchema, (result, c) => {
+//         if (!result.success) {
+//             return c.json(result.error, 400);
+//         }
+//     }), updateUserController)
+//     .delete("users/:id", deleteUserController);
+
 import { Hono } from 'hono';
 import { getUsersController, getUserByIdController, createUserController, updateUserController, deleteUserController } from './user.controller';
 import { zValidator } from '@hono/zod-validator';
-// Import the userSchema from the validators module
 import { userSchema } from '../validator';
 import { adminRoleAuth, userRoleAuth, bothRoleAuth } from './../middlewares/auth.middleware';
 
 export const userRouter = new Hono();
 
-// get all users
+// get all users - accessible by users and admins
 userRouter
     .get("users", userRoleAuth, getUsersController)
-    .post("users", zValidator('json', userSchema, (result, c) => {
+    .post("users", adminRoleAuth, zValidator('json', userSchema, (result, c) => {
         if (!result.success) {
             return c.json(result.error, 400);
         }
-    }), createUserController)
+    }), createUserController);
 
-// get user by id
+// get user by id - accessible by both users and admins
 userRouter
     .get("users/:id", bothRoleAuth, getUserByIdController)
-    .put("users/:id", zValidator('json', userSchema, (result, c) => {
+    .put("users/:id", adminRoleAuth, zValidator('json', userSchema, (result, c) => {
         if (!result.success) {
             return c.json(result.error, 400);
         }
     }), updateUserController)
-    .delete("users/:id", deleteUserController);
+    // Restrict DELETE route to admins only
+    .delete("users/:id", adminRoleAuth, deleteUserController);
+
+export default userRouter;
