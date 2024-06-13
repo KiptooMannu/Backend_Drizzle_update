626bcf147a5e909c6cce82fe318e4f51fdc6e05d
KiptooMannu <Kkgg7241@gmail.com>
2024-06-13 11:27:51 +0300

    usersmenuorders.router

--- src/MenuOrderUsers/UsersMenuOrders.Router.ts
+++ src/MenuOrderUsers/UsersMenuOrders.Router.ts
@@ -4,6 +4,6 @@ import { getUsersOrdersMenuItemsController } from './UsersMenuOrders.Controller'
 export const orderUsersMenuRouter = new Hono();
 
 // Get all users, their respective orders, and menu items
-orderUsersMenuRouter.get('/users-orders-menu-items', getUsersOrdersMenuItemsController);
+orderUsersMenuRouter.get('/(users)-(orders)-(menu-items)', getUsersOrdersMenuItemsController);
 
 export default orderUsersMenuRouter;
